a50a8ab6a8dd78ca691be05df35c3f45f21fce9c
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-23 15:37:43 +0700

    ss5 bai 2

---
+++ ss5 bai 2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	while(1)
+	{
+		printf("Moi ban doan so bat ki");
+		scanf("%d", &n);
+		if(n==30)
+		{
+			printf("dung roi");
+			
+		}
+		else 
+		{
+			printf("Nhap lai \n");
+		}
+	}
+	
+	return 0;
+	
+}
